63b8b62f17763459aa69d2bcc22b4b25a82c7531
Razvan Rotundu <raz.rotundu@gmail.com>
2024-10-03 11:21:24 -0400

    Complete GameRow class + tests

--- lib/game_row.rb
+++ lib/game_row.rb
@@ -2,19 +2,35 @@
 class GameRow
   attr_accessor :guess, :hint
 
-  def initialize(guess_string)
+  def initialize(guess_string = "----")
+    @guess = guess_string.chars
+    @hint = "----".chars
     
   end
 
-  def guess_write(string)
+  def guess_write(new_guess)
+    g = new_guess.chars
+    if g.length != 4
+      return false
+    else
+      @guess = g
+      return true
+    end
 
   end
 
-  def hint_write(string)
-
+  def hint_write(new_hint)
+    h = new_hint.chars
+    if h.length != 4
+      return false
+    else
+      @hint = h
+      return true
+    end
   end
 
   def to_s()
+    return("Guess: #{@guess.join(" ")}   Hint: #{@hint.join()}")
 
   end
 
--- spec/game_row_rspec.rb
+++ spec/game_row_rspec.rb
@@ -21,21 +21,21 @@ describe GameRow do
     r = GameRow.new()
 
     it "modifies the guess value with guess_write" do
-      r.guess_write("a b c d")
+      r.guess_write("abcd")
       expect(r.guess).to eql(['a', 'b', 'c', 'd']) 
     end
 
     it "overrwites current guess value with new guess value" do
-      r.guess_write("x x x x")
+      r.guess_write("xxxx")
       expect(r.guess).to eql(['x', 'x', 'x', 'x'])
     end
 
     it "prevents too long guesses" do  
-      expect(r.guess_write("c c c c c")).to eql(false)
+      expect(r.guess_write("ccccc")).to eql(false)
     end
 
     it "prevents too short guesses" do
-      expect(r.guess_write("aaaa")).to eql(false)
+      expect(r.guess_write("a")).to eql(false)
     end
   end
 
@@ -43,29 +43,31 @@ describe GameRow do
     r = GameRow.new()
 
     it "adds hint_write into an empty hint variable" do
-      r.hint_write("r r r r")
+      r.hint_write("rrrr")
       expect(r.hint).to eq(['r', 'r', 'r', 'r'])
     end
 
     it "overwrites existing hint value" do
-      r.hint_write("x x x x")
+      r.hint_write("xxxx")
       expect(r.hint).to eq(['x', 'x', 'x', 'x'])
     end
 
     it "prevents hints too long" do
-      expect(r.hint_write('x x x x x')).to eql(false)
+      expect(r.hint_write('xxxxx')).to eql(false)
     end
 
     it "prevents hints too short" do
-      expect(r.hint_write('askjn')).to eql(false)
+      expect(r.hint_write('a')).to eql(false)
     end
   end
 
   describe "to_s testing" do
     r = GameRow.new()
-    r.guess_write("q w e r")
-    r.hint_write("y y w w")
-    expect(r.to_s()).to eql("Guess: q w e r   Hint: yyww")
+    r.guess_write("qwer")
+    r.hint_write("yyww")
+    it "Converts to string properly" do
+      expect(r.to_s()).to eql("Guess: q w e r   Hint: yyww")
+    end
   end
 
 end
